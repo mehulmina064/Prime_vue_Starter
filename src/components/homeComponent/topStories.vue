615608f74da47956016cd8af3d08339152dad734
Mehul MIna <100404296+mehulmina064@users.noreply.github.com>
2023-08-25 20:04:47 +0530

    added

--- src/components/homeComponent/topStories.vue
+++ src/components/homeComponent/topStories.vue
@@ -1,3 +1,5 @@
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
 <!-- eslint-disable no-unused-vars -->
 <!-- eslint-disable prettier/prettier -->
 <script setup>
@@ -9,6 +11,7 @@ const dataviewValue = ref(null);
 const layout = ref('grid');
 const sortOrder = ref(null);
 const sortField = ref(null);
+const openCommentsDrawer  = ref(false);
 
 const data=ref({
     topStories:[],
@@ -16,6 +19,12 @@ const data=ref({
     commentsOnStory:[],
 });
 
+function openComments(id){
+    useHnaStore().getCommentsOnStory(id).then((res)=> {
+        data.value.commentsOnStory = [...res]
+        openCommentsDrawer.value = true
+    })
+}
 
 
 const hsnService = new HsnService();
@@ -24,13 +33,14 @@ onMounted(() => {
     useHnaStore().getTopStoriesData().then((api_data) => (data.value.topStories=api_data));
     useHnaStore().getPastStoriesData().then((api_data) => (data.value.pastStories=api_data));
 });
+
 </script>
 
 <template>
     <div class="grid">
         <div class="col-12">
             <div class="card">
-                <DataView :value="dataviewValue" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
+                <DataView :value="data.topStories" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
                     <template #header>
                         <div class="grid grid-nogutter" style="display: flex; align-items: center; justify-content: space-between">
                             <h4 style="margin: 0">Top Stories</h4>
@@ -63,28 +73,62 @@ onMounted(() => {
                         <div class="col-12 md:col-4">
                             <div class="card m-3 border-1 surface-border">
                                 <div class="flex align-items-center justify-content-between">
-                                    <div class="flex align-items-center">
-                                        <i class="pi pi-tag mr-2"></i>
-                                        <span class="font-semibold">{{ slotProps.data.category }}</span>
+                                    <div class="flex align-items-center mb-2">
+                              
+                                        <span class="font-bold">{{ slotProps.data.title }}</span>
                                     </div>
-                                    <span :class="'product-badge status-' + slotProps.data.inventoryStatus.toLowerCase()">{{ slotProps.data.inventoryStatus }}</span>
+                                </div>
+                                
+                               
+                                <div class="mb-2">Total comments: {{ slotProps.data.totalComments }}</div>
+                                <div>
+                                    <div>
+                                      <p class="mb-2">
+                                        Votes: {{ slotProps.data.votes }}
+                                      </p> 
+                                    </div>
+                                    <div class="mb-2">
+                                        Date: {{ slotProps.data.time }}
+                                    </div>
+                                </div>
+                                <div class="flex align-items-center justify-content-center m-4">
+                                    <Button label="Comments" :disabled="!slotProps.data.commentsIds" severity="info" @click="openComments(slotProps.data.id)"></Button>
                                 </div>
                                 <div class="text-center">
-                                    <img :src="'demo/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-9 shadow-2 my-3 mx-0" />
-                                    <div class="text-2xl font-bold">{{ slotProps.data.name }}</div>
-                                    <div class="mb-3">{{ slotProps.data.description }}</div>
-                                </div>
-                                <div class="flex align-items-center justify-content-between">
-                                    <span> Votes:${{ slotProps.data.price }}</span>
-                                    <!-- <span class="text-2xl font-semibold"> Votes${{ slotProps.data.price }}</span> -->
-                                    <!-- <Button icon="pi pi-shopping-cart" :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"></Button> -->
-                                    <Button label="Comments" :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'" severity="info"></Button>
+                                    <a :href="slotProps.data.url" target="_blank">See Story</a>
                                 </div>
                             </div>
+
+                            
                         </div>
                     </template>
                 </DataView>
             </div>
         </div>
+        <Dialog v-model:visible="openCommentsDrawer" modal header="Comments" :style="{ width: '50vw' }">
+            <div v-for="(data, index) in data.commentsOnStory" :key="index">
+                <Card class="mb-4">
+        <template #title>
+            <p> {{ data.createdBy.userName }}</p>
+             
+             </template>
+             <template #content>
+    <p>
+        <b class="text-lg">
+            Comment:
+        </b>
+        <span v-html="data.text"></span>
+    </p>
+    <div class="flex justify-content-end">
+        <p>
+            <b>Total Reply</b> {{ data.totalComments }}
+        </p>
+    </div>
+</template>
+    </Card>
+
+            </div>
+
+</Dialog>
     </div>
 </template>
